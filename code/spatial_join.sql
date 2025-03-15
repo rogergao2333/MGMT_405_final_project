9462288b1058bd7917fe2b42fda4432e1765f82f
rogergao2333 <rogergao2333@g.ucla.edu>
2025-03-14 20:35:29 -0700

    Update spatial_join.sql

--- code/spatial_join.sql
+++ code/spatial_join.sql
@@ -6,11 +6,11 @@ LOAD spatial;
 -- Create Airbnb and Yelp tables
 CREATE TABLE airbnb AS 
 SELECT *, ST_Point(longitude, latitude) AS geom 
-FROM read_csv_auto('team21/output/airbnb_cleaned_final.csv');
+FROM read_csv_auto('/home/ubuntu/team21/output/airbnb_cleaned_final.csv');
 
 CREATE TABLE yelp AS 
 SELECT *, ST_Point(longitude, latitude) AS geom 
-FROM read_csv_auto('team21/output/yelp_cleaned_final.csv');
+FROM read_csv_auto('/home/ubuntu/team21/output/yelp_cleaned_final.csv');
 
 -- Compute distances between all Airbnb and Yelp restaurants
 COPY (
@@ -20,4 +20,4 @@ COPY (
         ST_Distance(a.geom, y.geom) * 100 AS distance_km
     FROM airbnb a
     CROSS JOIN yelp y
-) TO 'team21/output/airbnb_yelp_distances.csv' WITH (HEADER, DELIMITER ',');
+) TO '/home/ubuntu/team21/output/airbnb_yelp_distances.csv' WITH (HEADER, DELIMITER ',');
